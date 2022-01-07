bf0646b652e4873d284698788a13879129814a98
gilbert.hoevel <gilbert.hoevel@stud.uni-goettingen.de>
2022-01-07 16:01:33 +0100

    created user area style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,38 @@
-@extends('layouts.app')
+@extends('/auth.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<body class="text-center">
+    <div class="row">
+        <div class="col-lg-5 col-md-7 col-sm-11 bg-white py-2 align-middle border border-light" style="float: none; margin: auto; margin-top: 10%; max-width: 350px;">
+            
+            <form class="form-signin m-2" method="POST" action="{{ route('login') }}">
+                @csrf
+     
+                <i class="fas fa-coffee -primary mb-2" style="width=172px; height=172px;"></i>
+                
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror mb-2" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-Mail">
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+      
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Passwort">
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            
+                <button type="submit" class="btn btn-lg btn-primary py-0 mt-2">
+                    <i class="bi bi-box-arrow-in-right"></i> {{ __('Login') }}
+                </button>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                <!-- only used for development and testing system -->
+                <p class="mt-3 mb-3 text-muted">Strichliste</p><p class="mt-1 text-muted bg-warning text-danger">ENTWICKLUNGSSYSTEM</p>
+            </form>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
         </div>
     </div>
-</div>
+</body>
 @endsection
---
+++ resources/views/user/master.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    @yield('head')
+
+</head>
+
+<body id="page-top" >
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-2 static-top border-bottom" style="border-color: #BABABA">
+        <!-- nav bar fpr icons on the left -->
+        <div class="container container-fluid m-0">
+            <div class="row">
+                <div class="col">
+                    <a href="" class="mr-1" data-toggle="tooltip" title="Home"><i class="bi bi-house-door fa-2x"></i></a>
+                </div>
+                <div class="col">
+                    <a href="" class="mr-1" data-toggle="tooltip" title="Warenkorb"><i class="bi bi-cart fa-2x"></i></a>
+                </div>
+                <!--<div class="col">
+                    <a href="" class="m-1" data-toggle="tooltip" title="Ident Verfahren"><i class="bi bi-upc-scan fa-2x"></i></a>
+                </div>!-->
+                <div class="col">
+                    <a href=""  class="mr-1" data-toggle="tooltip" title="Abrechnung"><i class="bi bi-clipboard-x fa-2x"></i></a>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- navbar on the right -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button type="button" class="btn btn-success btn-circle"></button>
+                </a>
+                       
+                <!-- dropdown menu user icon -->
+                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown"> 
+                    <a class="dropdown-item" href="">
+                        <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>Profil
+                    </a>
+                                
+                    <a class="dropdown-item" href="">
+                        <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>Admin Bereich
+                    </a>
+                                
+                    <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();"><i class="bi bi-box-arrow-right"></i> Logout</a>    
+                        <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+                    </div>
+            </li>
+        </ul>
+
+    </nav>
+                
+
+    <main class="py-0">
+        @yield('content')
+    </main>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
+
+</body>
+
+</html>
