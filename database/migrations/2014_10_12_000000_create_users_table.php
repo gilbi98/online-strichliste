60fab2c337b15b165c2f273e80fca951c9282394
gilbert.hoevel <gilbert.hoevel@stud.uni-goettingen.de>
2022-01-07 16:12:58 +0100

    added firstname, lastname, nickname, credit, role and private code to user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,17 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('nickname');
             $table->string('email')->unique();
+            $table->double('guthaben')->default(0);
+            $table->integer('role')->default(0);
+            $table->integer('pc1')->nullable();
+            $table->integer('pc2')->nullable();
+            $table->integer('pc3')->nullable();
+            $table->integer('pc4')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -24,11 +27,6 @@ class CreateUsersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
